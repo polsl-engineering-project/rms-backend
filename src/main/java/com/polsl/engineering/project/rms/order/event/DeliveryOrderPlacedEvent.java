743e5d8aeeff41e353f692e23beddc8ad5740b07
karixdev <kari.klucz@gmail.com>
2025-11-09 01:19:18 +0100

    feat: publish event

---
+++ src/main/java/com/polsl/engineering/project/rms/order/event/DeliveryOrderPlacedEvent.java
@@ -0,0 +1,21 @@
+package com.polsl.engineering.project.rms.order.event;
+
+import com.polsl.engineering.project.rms.order.vo.OrderId;
+import com.polsl.engineering.project.rms.order.vo.OrderLine;
+import com.polsl.engineering.project.rms.order.vo.Address;
+import com.polsl.engineering.project.rms.order.vo.CustomerInfo;
+
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.List;
+
+public record DeliveryOrderPlacedEvent(
+        OrderId orderId,
+        Instant placedAt,
+        List<OrderLine> lines,
+        Address deliveryAddress,
+        CustomerInfo customerInfo,
+        LocalTime scheduledFor
+) implements OrderEvent {
+}
+
